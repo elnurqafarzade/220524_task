1d371816c0a18afe78b99dee01fcc6827d78ae50
elnurqafarzade <morganofficial545@gmail.com>
2024-05-22 12:01:32 +0400

    Add files via upload

---
+++ 220524_task/22052024_task/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22052024_task
+{
+   
+    public class Car
+    {
+        public string Brand;
+        public string Model;
+        public int CurrentFuel;
+        public int Fuelfor1km;
+        public double Mileage;
+
+        public void Drive(int km)
+        {
+            if (CurrentFuel > km)
+            {
+                CurrentFuel = CurrentFuel - (Fuelfor1km * km);
+                Console.WriteLine($"Getdiyimiz yol: {km} km. Qalan yanacaq: {CurrentFuel} litr. Ümumi yürüş: {Mileage + km} km.");
+            }
+            else
+            {
+                Console.WriteLine("Masinda seyahete yetecek qeder benzin yoxdur.");
+            }
+        }
+    }
+    
+
+
+}
+
---
+++ 220524_task/22052024_task/Program.cs
@@ -0,0 +1,19 @@
+﻿using _22052024_task;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Channels;
+public class Program
+{
+
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+        car.Brand = "Chevrolet";
+        car.Model = "Camaro";
+        car.CurrentFuel = 60;
+        car.Mileage = 100000;
+        car.Fuelfor1km = 6;
+
+        car.Drive(10);
+
+    }
+}
